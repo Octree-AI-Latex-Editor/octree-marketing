348cb8d240a6229237a93a8650d269200183b85a
Zain Sajid <zsajid.bese19seecs@seecs.edu.pk>
2025-10-15 02:39:38 +0200

    remove migrations

--- src/migrations/index.ts
+++
@@ -1,9 +0,0 @@
-import * as migration_20251015_003655 from './20251015_003655';
-
-export const migrations = [
-  {
-    up: migration_20251015_003655.up,
-    down: migration_20251015_003655.down,
-    name: '20251015_003655'
-  },
-];

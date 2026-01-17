60f62cdf35be5e702a96c4138f1e0f6f4ac5321d
Basil Yusuf <basilyusuf1709@gmail.com>
2026-01-17 18:05:53 -0500

    fix: preview

--- src/utilities/mergeOpenGraph.ts
+++ src/utilities/mergeOpenGraph.ts
@@ -7,7 +7,7 @@ const defaultOpenGraph: Metadata['openGraph'] = {
     'The open-source LaTeX editor that makes academic writing feel natural and effortless',
   images: [
     {
-      url: `${getServerSideURL()}/website-template-OG.webp`,
+      url: `${getServerSideURL()}/images/octree-chat.png`,
     },
   ],
   siteName: 'Octree',

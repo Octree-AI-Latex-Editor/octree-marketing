8a2b4fa40f4076a1951baf1e6ed111ea54417abd
Basil Yusuf <basilyusuf1709@gmail.com>
2026-01-24 13:57:00 -0500

    feat: add logo

--- src/app/(frontend)/layout.tsx
+++ src/app/(frontend)/layout.tsx
@@ -96,6 +96,11 @@ export default async function RootLayout({ children }: { children: React.ReactNo
           suppressHydrationWarning
           type="application/ld+json"
         />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `function initApollo(){var n=Math.random().toString(36).substring(7),o=document.createElement("script");o.src="https://assets.apollo.io/micro/website-tracker/tracker.iife.js?nocache="+n,o.async=!0,o.defer=!0,o.onload=function(){window.trackingFunctions.onLoad({appId:"697514dfd884d00011e90a5d"})},document.head.appendChild(o)}initApollo();`,
+          }}
+        />
       </head>
       <body>
         <Providers>
